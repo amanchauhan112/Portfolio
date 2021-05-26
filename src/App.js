9e5c9da8440ccac8427ab060613b11116c9a3e86
Aman <amanchauhan2801@gmail.com>
2021-05-27 02:05:08 +0530

    will add other page and make one-pager

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from './pages/home'
 import About from './pages/about';
-import Education from './pages/education';
-import Skills from './pages/skills';
-import Profile_Links from './pages/profile_links';
 import Projects from './pages/projects';
 import Footer from './components/footer/footer';
 function App() {
@@ -18,13 +15,13 @@ function App() {
     <Navbar/>
 
     <Switch>
-      {/* <Route path='/education' component={Education} /> */}
       <Route path="/" exact component={Home}></Route>
       <Route path='/about' component={About} />
-      {/* <Route path='/skills' component={Skills} /> */}
       <Route path='/projects' component={Projects} />
-      {/* <Route path='/profile_links' component={Profile_Links} /> */}
     </Switch>
+    {/* <About/> */}
+
+    {/* <Projects/> */}
     {/* <Footer/> */}
     </Router>
   );
